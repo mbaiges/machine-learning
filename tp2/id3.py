748560bc535d341c290b732eedc8fea8388e5a90
Matías <mbaiges@itba.edu.ar>
2022-04-14 23:30:56 -0300

    feat(id3): gain function selector

--- tp2/id3.py
+++ tp2/id3.py
@@ -5,7 +5,7 @@ def shannon_entropy(x) -> float:
 
 class Node:
 
-    def __init__(self, name: str, childs: dict={}, value: int=None):
+    def __init__(self, name: str, childs: dict={}, value: int=None) -> None:
         self.name = name
         self.value = None
         self.childs = childs
@@ -15,10 +15,18 @@ class Node:
 
 class ID3:
             
-    def __init__(self) -> None:
+    def __init__(self, gain_f: str='shannon') -> None:
+        self._choose_gain(gain_f)
+
+    def _choose_gain(self, gain_f: str='shannon'):
+        # if gain_f == 'name_a_gain_function':
+        #     self.gain = function
+        # else: # if gain_f == 'shannon'
+        #     self.gain = shannon_entropy
         self.gain = shannon_entropy
 
     def _generate_tree(self) -> None:
+        print("Generating tree")
         pass
 
     def load(self, x: np.ndarray, t: np.ndarray) -> None:
