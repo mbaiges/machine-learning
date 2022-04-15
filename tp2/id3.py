c47007a377b1d080d1a0a11a1aea01b6a8c04a0e
Matías <mbaiges@itba.edu.ar>
2022-04-15 13:07:02 -0300

    feat(id3): started generating tree using gain
    Co-authored-by: Scott Lin <slin@itba.edu.ar>
    Co-authored-by: Camila Ponce <cponce@itba.edu.ar>

--- tp2/id3.py
+++ tp2/id3.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import copy
+import math
 
-def shannon_entropy(x) -> float:
-    pass
+from utils import mode
+
+def shannon_entropy(df: pd.DataFrame, atr: str) -> float:
+    atr_values = df[atr].unique()
+    total = df.size
+    entropy = 0
+    for v in atr_values:
+        rel_freq = df[df[atr] == v].size / total
+        entropy -=  rel_freq * math.log(rel_freq, 2)
+    return entropy
 
 class Stack:
-
     class ValuedAttribute:
         def __init__(self, name: str, value: int) -> None:
             self.name = name
@@ -23,12 +31,16 @@ class Stack:
     def path(self) -> list:
         return copy.deepcopy(self.l)
 
+    def __iter__(self):
+        return self.path()
+
 class Node:
 
-    def __init__(self, name: str, childs: dict={}, value: int=None) -> None:
+    def __init__(self, name: str, childs: dict={}, value: int=None, depth: int=None) -> None:
         self.name = name
-        self.value = None
         self.childs = childs
+        self.value = value
+        self.depth = depth
 
     def is_leaf(self) -> bool:
         return len(self.childs.keys()) and self.value is None
@@ -38,21 +50,70 @@ class ID3:
     def __init__(self, gain_f: str='shannon') -> None:
         self._choose_gain(gain_f)
 
-    def _choose_gain(self, gain_f: str='shannon'):
+    def _choose_gain(self, gain_f: str='shannon') -> None:
         # if gain_f == 'name_a_gain_function':
         #     self.gain = function
         # else: # if gain_f == 'shannon'
         #     self.gain = shannon_entropy
-        self.gain = shannon_entropy
+        self.gain_method = shannon_entropy
 
+    def gain(self, df: pd.DataFrame, atr: str) -> float:
+        gain = self.gain_method(df, self.target_atr)
+        total = df.size
+        atr_values = df[atr].unique()
+
+        for value in atr_values:
+            gain -= (df[df[atr] == value].size / total) * self.gain_method(df[df[atr] == value], self.target_atr)
+        return gain
+        
+    def _generate_node(self, atr: str, stack: Stack, pending: set, depth: int) -> None:
+        if pending:
+            df = self._get_filtered_dataframe(stack) 
+            obj_values = df[self.target_atr].unique()
+            if len(obj_values) == 1:
+                return Node(atr, value=obj_values[0], depth=depth)
+            max_gain = None
+            for atr in pending:
+                gain = self.gain(df, atr)
+                if max_gain is None or gain > max_gain[1]:
+                    max_gain = (atr, gain)
+            atr_values = df[atr].unique()
+            childs = {}
+            new_pending = copy.deepcopy(pending)
+            # new_pending.discard(max_gain[0])
+            print(f"discarding: {new_pending.discard(max_gain[0])}")
+            print(new_pending)
+            for value in atr_values:
+                stack.push(Stack.ValuedAttribute(max_gain[0], value))
+                print("Hola")
+                childs[value] = self._generate_node(max_gain[0], stack, new_pending, depth+1)
+                stack.pop()
+            return Node(max_gain[0], childs=childs, depth=depth)
+        else:
+            return Node(atr, value=self.s_mode(stack), depth=depth)
+
+    def s_mode(self, stack: Stack) -> float:
+        df = self._get_filtered_dataframe(stack)
+        return mode(df[self.target_atr])
+    
+    def _get_filtered_dataframe(self, stack: Stack) -> pd.DataFrame:
+        p = stack.path()
+        recorte = self.examples
+        for v_atr in p:
+            print(f"{v_atr.name} - {v_atr.value}")
+            recorte = recorte[recorte[v_atr.name] == v_atr.value]
+        return recorte
+    
     def _generate_tree(self) -> None:
         print("Generating tree")
-        pass
+        self.tree = self._generate_node(self.target_atr, Stack(), set(self.x.columns), depth=0)
 
     def _find_most_gain_att(self, x: pd.DataFrame, t: pd.DataFrame) -> str:
         pass
 
     def load(self, x: pd.DataFrame, t: pd.DataFrame) -> None:
+        self.examples = pd.concat([x, t], axis=1)
         self.x = x
         self.t = t
+        self.target_atr = self.t.columns[0]
         self._generate_tree()
