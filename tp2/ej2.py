ej3 & knn: fix update method and normalization

tp2/ej2.py
@@ -68,7 +68,7 @@ def sanitize(df):
         regs = regs[regs[TITLE_SENTIMENT].notnull()]
         for text_sent in text_sentiments:
             rs = regs[regs[TEXT_SENTIMENT] == text_sent]
-            mo = mode(list(map(lambda i: i[1][TITLE_SENTIMENT], rs.iterrows())))
+            mo = mode(np.array(list(map(lambda i: i[1][TITLE_SENTIMENT], rs.iterrows()))))
             mo = mo if mo is not None else text_sent
             mode_by_stars_and_text_sent[(s, text_sent)] = mo
     
@@ -205,7 +205,7 @@ def confusion(all_results):
 
 def compare_weighted(x, t, min_k=1, max_k=200, step_k=4, iterations_per_k=4):
     # non_weighted
-    ks = list(range(min_k, max_k, step_k))
+    ks = list(range(min_k, max_k + step_k, step_k))
 
     non_weighted_precisions = []
     weighted_precisions     = []
@@ -261,5 +261,5 @@ if __name__ == '__main__':
     print(precision)
     confusion(all_results)
 
-    # compare_weighted(x, t, min_k=1, max_k=50, step_k=1, iterations_per_k=4)
+    compare_weighted(x, t, min_k=5, max_k=200, step_k=5, iterations_per_k=4)
     

tp2/knn.py
@@ -45,7 +45,7 @@ class KNN:
                 return curr + 1
             elif only_zeros and dist != 0:
                 return curr
-            return curr + dist
+            return curr + 1 / dist
         most_frequent_found = False
         k = self.k
         only_zeros = False
@@ -69,14 +69,15 @@ class KNN:
                         max_n = 0
                         only_zeros = True
                 f = update_method(freq.get(e['t'], 0), dist, only_zeros=only_zeros)
+                # print(f"f is {f}")
                 if f > 0 or not only_zeros:
                     freq[e['t']] = f
-                if max == None or (max != e['t'] and freq[e['t']] >= freq[max]):
-                    if max != None and freq[e['t']] == freq[max]:
-                        max_n += 1
-                    else:
-                        max_n = 1
-                        max = e['t']
+                    if max == None or (max != e['t'] and freq[e['t']] >= freq[max]):
+                        if max != None and freq[e['t']] == freq[max]:
+                            max_n += 1
+                        else:
+                            max_n = 1
+                            max = e['t']
 
             # Check if there is a most_freq
             if max_n == 1:
@@ -102,8 +103,8 @@ class KNN:
     def find(self, x):
         res = []
         for e in x:
-            e = np.array(e)
-            closest = self._closest(self.nx(e))
+            e = self.nx(np.array(e))
+            closest = self._closest(e)
             # print(self._denorm_w(closest))
             max, probs, k = self._most_frequents_for_k(e, closest)
             res.append((